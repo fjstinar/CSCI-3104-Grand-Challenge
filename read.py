d8078c7bd6024a8e59bc2320cc055c9b7558a754
Cole Sturza <cole@getcrossbeam.com>
2019-04-22 21:17:40 -0600

    Add file to read FASTQ file.

---
+++ read.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+def process(lines=None):
+    ks = ['name', 'sequence', 'optional', 'quality']
+    return {k: v for k, v in zip(ks, lines)}
+
+try:
+    fn = sys.argv[1]
+except IndexError as ie:
+    raise SystemError("Error: Specify file name\n")
+
+if not os.path.exists(fn):
+    raise SystemError("Error: File does not exist\n")
+
+
+n = 4
+records = []
+with open(fn, 'r') as fh:
+    lines = []
+    for line in fh:
+        lines.append(line.rstrip())
+        if len(lines) == n:
+            records.append(process(lines))
+            lines = []
